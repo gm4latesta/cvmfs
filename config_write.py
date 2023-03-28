0977f0f9b2b5a6d60ae209642e159ba47878e263
Giada Malatesta <giada.malatestavt@gmail.com>
2023-03-28 11:29:01 +0200

    Updating configuration file

--- config_write.py
+++ config_write.py
@@ -3,12 +3,21 @@ import configparser
 config = configparser.ConfigParser()
 
 config['database'] = {
-    'OIDC_PROFILE_NAME': 'infncloud',
-    'ENDPOINT_URL': 'https://minio.cloud.infn.it/',
-    'USER_NAME': 'gmalatesta',
-    'HOST': 'minio.cloud.infn.it'
+    'OIDC_PROFILE_NAME' : 'infncloud',
+    'ENDPOINT_URL' : 'https://minio.cloud.infn.it/',
+    'USER_NAME' : 'gmalatesta',
+    'HOST' : 'minio.cloud.infn.it'
 }
 
-with open('cvmfs.cfg', 'w') as config_file:
+config['default'] = {
+    'host_base' : 'minio.cloud.infn.it' ,
+    'host_bucket' : 'minio.cloud.infn.it' ,
+    'use_https' : 'true' , 
+    'access_key' : '' , 
+    'secret_key' : ''
+
+}
+
+with open('s3_cvmfs.cfg', 'w') as config_file:
     config.write(config_file)
 
