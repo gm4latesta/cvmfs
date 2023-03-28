54ffda4d8224424a4b90e7998be961e18e7046db
Giada Malatesta <giada.malatestavt@gmail.com>
2023-03-28 15:21:02 +0200

    Updating sync_repo.py

--- sync_repo.py
+++ sync_repo.py
@@ -1,36 +1,36 @@
-from boto3sts import credentials as creds
 import boto3
 import subprocess
 import configparser
 import logging 
 
 
-def get_names(OIDC_PROFILE_NAME,ENDPOINT_URL) :
+def get_names(ACCESS_KEY,SECRET_KEY,ENDPOINT_URL) :
 
     '''This functions returns the names of the buckets with cvmfs/ area and the credentials needed to download their content  '''
     
-    #Getting refreshble credentials session
-    credentials = creds.s3_session_credentials(OIDC_PROFILE_NAME, endpoint=ENDPOINT_URL, verify=True)
-    aws_session = creds.assumed_session(OIDC_PROFILE_NAME)
-    s3 = aws_session.client('s3', endpoint_url=ENDPOINT_URL, 
-                                config=boto3.session.Config(signature_version='s3v4'),
-                                verify=True)
+    s3 = boto3.client('s3', endpoint_url=ENDPOINT_URL, 
+                        config=boto3.session.Config(signature_version='s3v4'),
+                        verify=True , 
+                        aws_access_key_id=ACCESS_KEY,
+                        aws_secret_access_key=SECRET_KEY)
 
     names=[]
     for bkt in s3.list_buckets()['Buckets']: 
-        for obj in s3.list_objects(Bucket=bkt['Name'])['Contents']:
-            if 'cvmfs/' in obj['Key']:
-                names.append(bkt['Name'])   #questo non funziona se il nome è dentro scratch e va estratto da obj['Key']
-            break 
+        if 'Contents' in s3.list_objects(Bucket=bkt['Name']).keys():
+            for obj in s3.list_objects(Bucket=bkt['Name'])['Contents']:
+                if 'cvmfs/' in obj['Key']:
+                    names.append(bkt['Name'])   
+                    break
+        else: continue  
 
-    return names , credentials 
+    return names  
 
 
 def transaction(bucket):
 
     '''This function starts a transaction in the repositery in stratum-0'''
 
-    print('Starting transaction..')
+    print('Starting transaction...')
     cmd = 'cvmfs_server transaction %s.infn.it'  %bucket   
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
@@ -38,15 +38,11 @@ def transaction(bucket):
     return True 
         
 
-def sync_repo(credentials,bucket,HOST):
+def sync_repo(ACCESS_KEY,SECRET_KEY,bucket):
 
     '''This functions syncronizes the repo in stratum-0 with the s3 bucket'''
 
-    ACCESS_KEY = credentials["access_key"]
-    SECRET_KEY = credentials["secret_key"]
-    TOKEN = credentials["token"]
-
-    cmd = 's3cmd --access_key=%s --secret_key=%s --access_token=%s --host=%s --host-bucket=%s sync s3://%s/cvmfs/ /cvmfs/%s.infn.it/' % (bucket,ACCESS_KEY,SECRET_KEY,TOKEN,HOST,HOST,bucket,bucket)
+    cmd = 's3cmd -c s3_cvmfs.cfg sync s3://%s/cvmfs/ /cvmfs/%s.infn.it/' % (bucket,bucket)
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
 	    logging.warning('Synchronization not succeded')
@@ -57,7 +53,7 @@ def publish(bucket):
     cmd= 'cvmfs_server publish %s.infn.it' %bucket
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
-        logging.warning('Unable to publish, aborting transaction..')
+        logging.warning('Unable to publish, aborting transaction...')
         cmd='cvmfs_server abort -f %s.infn.it' %bucket
         p=subprocess.run(cmd, shell=True)
         if p.returncode != 0:
@@ -69,29 +65,29 @@ def publish(bucket):
 if __name__ == '__main__' :
 
     config = configparser.ConfigParser()
-    config.read('cvmfs.cfg')
+    config.read('s3_cvmfs.cfg')
 
-    OIDC_PROFILE_NAME = config.get('database','OIDC_PROFILE_NAME')
+    root_logger= logging.getLogger()
+    root_logger.setLevel(logging.WARNING)
+
     ENDPOINT_URL = config.get('database','ENDPOINT_URL')
-    USER_NAME = config.get('database','USER_NAME')
-    HOST = config.get('database','HOST')
-
+    ACCESS_KEY = config.get('default','access_key')
+    SECRET_KEY = config.get('default','secret_key')
 
     #Get buckets names 
-    bkt_names,credentials=get_names(OIDC_PROFILE_NAME,ENDPOINT_URL)
+    bkt_names=get_names(ACCESS_KEY,SECRET_KEY,ENDPOINT_URL)
 
     #Sync alle the repo in cvmfs stratum-0 with the s3 buckets and publish the changes 
     for bkt in bkt_names:
-        root_logger= logging.getLogger()
-        root_logger.setLevel(logging.WARNING)
         handler = logging.FileHandler('/home/ubuntu/logs_cvmfs/%s.log' %USER_NAME, mode='w', encoding='utf-8', delay=True) 
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         root_logger.addHandler(handler)
-        tr=transaction(bkt)
+        tr = transaction(bkt)
         if tr==False:
-            logging.error('Unable to start transaction') 
+            logging.error('Unable to start transaction...') 
+            continue
         else:
-            sync_repo(credentials,bkt,HOST)
+            sync_repo(ACCESS_KEY,SECRET_KEY,bkt)
             publish(bkt)
 
         
