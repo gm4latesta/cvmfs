45911f457e7cda00ad3b35272ec7770fd94d61fb
Giada Malatesta <giada.malatestavt@gmail.com>
2023-05-02 11:18:00 +0200

    Updating sync_repo.py

--- sync_repo.py
+++ sync_repo.py
@@ -44,10 +44,10 @@ def transaction(bucket):
 
 def mkdir(bucket):
 
-    '''This function creates the folder /cvmfs/<username>.infn.it/tarballs_to_be_extracted if it does not exist'''
+    '''This function creates the folder /cvmfs/<username>.infn.it/software if it does not exist'''
 
-    if 'tarballs_to_be_extracted' not in os.listdir('/cvmfs/%s.infn.it' %bucket):
-        cmd = 'mkdir /cvmfs/%s.infn.it/tarballs_to_be_extracted' %bucket
+    if 'software' not in os.listdir('/cvmfs/%s.infn.it' %bucket):
+        cmd = 'mkdir /cvmfs/%s.infn.it/software' %bucket
         p=subprocess.run(cmd, shell=True)
     return
 
@@ -55,19 +55,19 @@ def mkdir(bucket):
 def sync_repo(bucket):
 
     '''This function syncronizes the cvmfs/ area in the s3 bucket with the repo in stratum-0,
-        and the cvmfs/tarballs_to_be_extracted area in s3 bucket with /tmp/sofwtare directory in stratum 0. '''
+        and the cvmfs/software area in s3 bucket with /home/ubuntu/sofwtare directory in stratum 0. '''
 
     #Synchronization of the cvmfs/ area of the bucket with the /cvmfs repo
-    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude 'tarballs_to_be_extracted/*' --delete-removed s3://%s/cvmfs/ /cvmfs/%s.infn.it/" % (bucket,bucket) 
+    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude 'software/*' --delete-removed s3://%s/cvmfs/ /cvmfs/%s.infn.it/" % (bucket,bucket) 
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
 	    logging.warning('Bucket and cvmfs repo synchronization not succeded\n', p.returncode)
 
-    #Synchronization of the cvmfs/tarballs_to_be_extracted/ area of the bucket with /tmp/software directory of stratum 0 
-    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude '*' --include '*.tar' s3://%s/cvmfs/tarballs_to_be_extracted/ /tmp/software/" % bucket
+    #Synchronization of the cvmfs/software/ area of the bucket with /home/ubuntu/software directory of stratum 0 
+    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude '*' --include '*.tar' --include '*.cfg' s3://%s/cvmfs/software/ /home/ubuntu/software/" % bucket
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
-	    logging.warning('Bucket and /tmp/software dir in stratum 0 synchronization not succeded\n', p.returncode)
+	    logging.warning('Bucket and /home/ubuntu/software dir in stratum 0 synchronization not succeded\n', p.returncode)
 
 
 def publish(bucket):
@@ -91,7 +91,7 @@ def distribute_software(bucket):
 
     '''This function looks for the software.cfg file in the repository, scans all its sections 
         and for each software it look at the 'publish' variable. If the value is 'yes', it takes the software in .tar format
-        from /tmp/software directory and distributes it using cvmfs method 'cvmfs_server ingest' in the
+        from /home/ubuntu/software directory and distributes it using cvmfs method 'cvmfs_server ingest' in the
         directory specified in the 'base_dir' variable of the .cfg file'''
 
     if '%s_software.cfg' %bucket in os.listdir('/home/ubuntu/software'):
@@ -104,32 +104,37 @@ def distribute_software(bucket):
                 new = config.get(section, 'new')
 
                 #The software has never be distributed 
-                if section not in os.listdir('/cvmfs/%s.infn.it/tarballs_to_be_extracted' %bucket):
-                    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir tarballs_to_be_extracted/%s/ %s.infn.it' %(section, base_dir, bucket)
+                if section not in os.listdir('/cvmfs/%s.infn.it/software' %bucket):
+                    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir software/%s/ %s.infn.it' %(section, base_dir, bucket)
                     p=subprocess.run(cmd, shell=True)
                     if p.returncode != 0:
                         logging.error('Unable to publish the server %s' %section )
 
                 #The software has already been distributed but there is a new version, the previous one is renamed as _old
-                elif section in os.listdir('/cvmfs/%s.infn.it/tarballs_to_be_extracted' %bucket) and new==True:
+                elif section in os.listdir('/cvmfs/%s.infn.it/software' %bucket) and new==True:
                     tr=transaction(bucket)
                     if tr==False:
                         continue 
-                    cmd = 'mv /cvmfs/%s.infn.it/tarballs_to_be_extracted/%s /cvmfs/%s.infn.it/tarballs_to_be_extracted/%s_old' %(bucket,section,bucket,section)
+                    cmd = 'mv /cvmfs/%s.infn.it/software/%s /cvmfs/%s.infn.it/software/%s_old' %(bucket,section,bucket,section)
                     p=subprocess.run(cmd, shell=True)
                     if p.returncode != 0:
                         logging.error(p.returncode)
                     pb=publish(bucket)
                     if pb == False:
                         continue 
-                    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir tarballs_to_be_extracted/%s/ %s.infn.it' %(section, base_dir, bucket)
+                    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir software/%s/ %s.infn.it' %(section, base_dir, bucket)
                     p=subprocess.run(cmd, shell=True)
                     if p.returncode != 0:
                         logging.error('Unable to publish the server %s' %section )
                     
             except Exception as ex:
-                logging.warning('Missing configuration info for %s in %s_software.cfg\n' %(section,bucket) , ex )
+                with open('/cvmfs/%s.infn.it/software/info_log.txt' , 'a') as file:
+                    file.write('Write correctly the configuration file for softare distribution (<username>_software.cfg), see documentation')
             
+    else:
+        with open('/cvmfs/%s.infn.it/software/info_log.txt' , 'a') as file:
+            file.write('Missing configuration file for software distribution (<username>_software.cfg), write it in the correct format if software distribution is needed')
+
 
 
 
@@ -139,9 +144,6 @@ if __name__ == '__main__' :
 
     config = configparser.ConfigParser()
     config.read('/home/ubuntu/s3_cvmfs.cfg')
-
-    root_logger= logging.getLogger()
-    root_logger.setLevel(logging.WARNING)
 
     ENDPOINT_URL = config.get('database','ENDPOINT_URL')
     ACCESS_KEY = config.get('default','access_key')
@@ -154,6 +156,8 @@ if __name__ == '__main__' :
     for bkt in bkt_names:
         handler = logging.FileHandler('/home/ubuntu/logs_cvmfs/%s.log' %bkt, mode='a', encoding='utf-8', delay=True) 
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        root_logger= logging.getLogger()
+        root_logger.setLevel(logging.INFO)
         root_logger.addHandler(handler)
         tr = transaction(bkt)
         if tr==False:
