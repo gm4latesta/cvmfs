1c96bb8f3c06642011787d4d0dadbd97273f807c
Giada Malatesta <giada.malatestavt@gmail.com>
2023-05-03 11:46:39 +0200

    Updating sync_repo.py

--- sync_repo.py
+++ sync_repo.py
@@ -30,6 +30,17 @@ def get_names(ACCESS_KEY,SECRET_KEY,ENDPOINT_URL) :
     return names  
 
 
+def sync_sw(bucket):
+
+    '''This function syncronizes the cvmfs/software/ folder in the s3 bucket of the user with /home/ubuntu/software
+    folder in stratum 0'''
+
+    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude '*' --include '*.tar' --include '*.cfg' s3://%s/cvmfs/software/ /home/ubuntu/software/" % bucket
+    p=subprocess.run(cmd, shell=True)
+    if p.returncode != 0:
+	    logging.warning('Bucket and /home/ubuntu/software dir in stratum 0 synchronization not succeded\n', p.returncode)
+
+
 def transaction(bucket):
 
     '''This function starts a transaction in the repositery in stratum-0'''
@@ -42,31 +53,16 @@ def transaction(bucket):
     return True 
 
 
-def mkdir(bucket):
-
-    '''This function creates the folder /cvmfs/<username>.infn.it/software if it does not exist'''
-
-    if 'software' not in os.listdir('/cvmfs/%s.infn.it' %bucket):
-        os.mkdir('/cvmfs/%s.infn.it/software' %bucket)
-    return
-
-
 def sync_repo(bucket):
 
-    '''This function syncronizes the cvmfs/ area in the s3 bucket with the repo in stratum-0,
+    '''This function syncronizes the cvmfs/ folder in the s3 bucket with the repo in stratum-0,
         and the cvmfs/software area in s3 bucket with /home/ubuntu/sofwtare directory in stratum 0. '''
 
     #Synchronization of the cvmfs/ area of the bucket with the /cvmfs repo
     cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude 'software/*' --delete-removed s3://%s/cvmfs/ /cvmfs/%s.infn.it/" % (bucket,bucket) 
     p=subprocess.run(cmd, shell=True)
     if p.returncode != 0:
-	    logging.warning('Bucket and cvmfs repo synchronization not succeded\n', p.returncode)
-
-    #Synchronization of the cvmfs/software/ area of the bucket with /home/ubuntu/software directory of stratum 0 
-    cmd = "s3cmd -c /home/ubuntu/s3_cvmfs.cfg sync --exclude '*' --include '*.tar' --include '*.cfg' s3://%s/cvmfs/software/ /home/ubuntu/software/" % bucket
-    p=subprocess.run(cmd, shell=True)
-    if p.returncode != 0:
-	    logging.warning('Bucket and /home/ubuntu/software dir in stratum 0 synchronization not succeded\n', p.returncode)
+	    logging.warning('Bucket and cvmfs repo synchronization not succeded\n', p.returncode)    
 
 
 def publish(bucket):
@@ -89,7 +85,7 @@ def publish(bucket):
 def distribute_software(bucket):
 
     '''This function looks for the software.cfg file in the repository, scans all its sections 
-        and for each software it looks for the veriables needed for the distribution'''
+        and for each software it looks for the variable needed for the distribution'''
 
     if '%s_software.cfg' %bucket in os.listdir('/home/ubuntu/software'):
         config = configparser.ConfigParser()
@@ -99,7 +95,6 @@ def distribute_software(bucket):
 
             try: 
                 base_dir = config.get(section,'base_dir')
-                new = config.get(section, 'new')
 
                 #The software has never be distributed 
                 if section not in os.listdir('/cvmfs/%s.infn.it/software' %bucket):
@@ -109,30 +104,26 @@ def distribute_software(bucket):
                         logging.error('Unable to publish the server %s' %section )
 
                 #The software has already been distributed but there is a new version, the previous one is renamed as _old
-                elif section in os.listdir('/cvmfs/%s.infn.it/software' %bucket) and new==True:
-                    tr=transaction(bucket)
-                    if tr==False:
-                        continue 
-                    cmd = 'mv /cvmfs/%s.infn.it/software/%s /cvmfs/%s.infn.it/software/%s_old' %(bucket,section,bucket,section)
-                    p=subprocess.run(cmd, shell=True)
-                    if p.returncode != 0:
-                        logging.error(p.returncode)
-                    pb=publish(bucket)
-                    if pb == False:
-                        continue 
-                    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir software/%s/ %s.infn.it' %(section, base_dir, bucket)
-                    p=subprocess.run(cmd, shell=True)
-                    if p.returncode != 0:
-                        logging.error('Unable to publish the server %s' %section )
+                #elif section in os.listdir('/cvmfs/%s.infn.it/software' %bucket) and new==True:
+                #    tr=transaction(bucket)
+                #    if tr==False:
+                #        continue 
+                #    cmd = 'mv /cvmfs/%s.infn.it/software/%s /cvmfs/%s.infn.it/software/%s_old' %(bucket,section,bucket,section)
+                #    p=subprocess.run(cmd, shell=True)
+                #    if p.returncode != 0:
+                #        logging.error(p.returncode)
+                #    pb=publish(bucket)
+                #    if pb == False:
+                #        continue 
+                #    cmd = 'cvmfs_server ingest --tar_file /home/ubuntu/software/%s.tar --base_dir software/%s/ %s.infn.it' %(section, base_dir, bucket)
+                #    p=subprocess.run(cmd, shell=True)
+                #    if p.returncode != 0:
+                #        logging.error('Unable to publish the server %s' %section )
                     
             except Exception as ex:
-                with open('/cvmfs/%s.infn.it/software/info_log.txt' , 'a') as file:
-                    file.write('Write correctly the configuration file for softare distribution (<username>_software.cfg), see documentation')
-            
+                return "error"
     else:
-        with open('/cvmfs/%s.infn.it/software/info_log.txt' , 'a') as file:
-            file.write('Missing configuration file for software distribution (<username>_software.cfg), write it in the correct format if software distribution is needed')
-
+        return "no cfg" 
 
 
 
@@ -156,24 +147,47 @@ if __name__ == '__main__' :
     #Create logs_cvmfs directory for storing logs 
     if 'logs_cvmfs' not in os.listdir('/home/ubuntu'):
         os.mkdir('/home/ubuntu/logs_cvmfs')
-
+        
     #Sync alle the repo in cvmfs stratum-0 with the s3 buckets and publish the changes 
     for bkt in bkt_names:
         handler = logging.FileHandler('/home/ubuntu/logs_cvmfs/%s.log' %bkt, mode='a', encoding='utf-8', delay=True) 
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         root_logger= logging.getLogger()
-        root_logger.setLevel(logging.INFO)
         root_logger.addHandler(handler)
         tr = transaction(bkt)
         if tr==False:
             logging.error('Unable to start transaction...') 
             continue 
-        mkdir(bkt)
+        
+        if 'software' not in os.listdir('/cvmfs/%s.infn.it' %bkt):
+            os.system('mkdir /cvmfs/%s.infn.it/software' %bkt)
+
+        #Synchronization
+        sync_sw(bkt)
         sync_repo(bkt)
         publish(bkt)
-        #Software distribution 
-        distribute_software(bkt)
 
+        #Software distribution 
+        sw=distribute_software(bkt)
+
+        if sw=="no cfg":
+            tr=transaction(bkt)
+            if tr==False:
+                continue 
+            with open('/cvmfs/%s.infn.it/software/info_log.txt' %bkt , 'w') as file:
+                file.write('Missing configuration file for software distribution (<username>_software.cfg), write it in the correct format if software distribution is needed\n')
+            publish(bkt)
+        
+        elif sw=="error":
+            tr=transaction(bkt)
+            if tr==False:
+                continue 
+            with open('/cvmfs/%s.infn.it/software/info_log.txt' %bkt , 'w') as file:
+                file.write('Missing base_dir variable in the configuration file (<username>_software.cfg), see documentation\n')
+            publish(bkt)
+
+    #Remove .tar files from Stratum 0
+    os.system('rm /home/ubuntu/software/*.tar')
 
     end_time = time.time()
     print("Execution time:", end_time - start_time, "seconds")
